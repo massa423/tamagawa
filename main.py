edeca026cf58bcee1d636cb869b64f27549163ee
Suzuki Masaya <vagrant@example.com>
2020-11-30 22:55:16 +0900

    コマンドをメソッド分割

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 
+from typing import List, Any
 from config import IP, PORT, LISTEN_NUM, BUFFER_SIZE
 
 
@@ -7,48 +8,88 @@ class CMD():
     """
     command class
     """
-    GET_ALL: bytes = b'GET_ALL\r\n'
-    QUIT: bytes = b'QUIT\r\n'
+    GET_ALL: str = 'GET_ALL'
+    QUIT: str = 'QUIT'
 
 
 class TCP_Server():
     """
     tcp server
     """
-
     def __init__(self):
         # IPv4/TCP
-        self.tcp_server: socket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM
-            )
+        self.tcp_server: socket = socket.socket(socket.AF_INET,
+                                                socket.SOCK_STREAM)
         self.tcp_server.bind((IP, PORT))
         self.tcp_server.listen(LISTEN_NUM)
+
+    def get_all(self, buffered_args: List) -> bytes:
+        """
+        get_all
+        """
+        print("GET_ALL: " + ' '.join(buffered_args))
+        if not buffered_args:
+            response = b"502 Bad Parameter"
+        else:
+            response = b"200 OK!"
+
+        return response
+
+    def process(self, cmd: CMD, buffered_args: List):
+        """
+        process
+        """
+        if cmd == CMD.GET_ALL:
+            response = self.get_all(buffered_args)
+
+        return response
 
     def __loop_and_wait(self, client, address):
         """
         loop and wait
         """
+        wait_for_dot: bool = False
+        cmd: CMD = None
+        buffered_args: List = []
+
         while True:
             break_flag: bool = False
             response: bytes = b""
-            data: bytes = client.recv(BUFFER_SIZE)
+            input: bytes = client.recv(BUFFER_SIZE)
 
-            if data == b"\xff\xf4\xff\xfd\x06":
+            if input == b"\xff\xf4\xff\xfd\x06":
                 print("SIGTERM is received.")
                 client.close()
                 break
 
+            data: bytes = input.strip(b'\r\n')
             print(b"[*] Received Data: " + data)
 
-            print(CMD.GET_ALL)
-            if data == CMD.GET_ALL:
-                response = b"200 OK!\n"
-                break_flag = True
-            elif data == CMD.QUIT:
-                response = b"QUIT\n"
-                break_flag = True
+            if wait_for_dot:
+                if data != b'.':
+                    response = b"522 Bad Request!"
+                    break_flag = True
+                else:
+                    wait_for_dot = False
+                    break_flag = True
 
-            client.send(response)
+                    response = self.process(cmd, buffered_args)
+
+            else:
+                cmd, *buffered_args = data.decode('ascii').split(' ')
+
+                if cmd == CMD.GET_ALL:
+                    wait_for_dot = True
+                    continue
+
+                elif cmd == CMD.QUIT:
+                    response = b"QUIT"
+                    break_flag = True
+
+                else:
+                    response = b"command is invalid"
+
+            client.send(response + b'\n')
 
             if break_flag:
                 client.close()
@@ -58,19 +99,20 @@ class TCP_Server():
         """
         run
         """
-        try:
-            while True:
+        client: socket = None
+        address: Any
+
+        while True:
+            try:
                 client, address = self.tcp_server.accept()
                 print("[*] Connectioned from : {}".format(address))
 
-                try:
-                    self.__loop_and_wait(client, address)
-                except KeyboardInterrupt:
+                self.__loop_and_wait(client, address)
+            except KeyboardInterrupt:
+                if client is not None:
                     print("Keyboad Interrupt. socket will close.")
                     client.close()
-                    break
-        except Exception as e:
-            print(e)
+                break
 
 
 t = TCP_Server()
